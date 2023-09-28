a496fd4fb5ac7ea812a68138c03dc441a349c7e0
iftakharul28 <iftakharul28@gmail.com>
2023-09-29 00:46:08 +0600

    layout file changed

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
+  <Header />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <Footer />
 </template>
--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Header></Header>
-  <slot></slot>
-  <Footer></Footer>
-</template>
-<script lang="ts">
-import Header from '~/components/Header.vue';
-import Footer from '~/components/Footer.vue';
-</script>
